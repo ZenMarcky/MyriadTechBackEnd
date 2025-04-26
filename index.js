90c673dd71ab9406ba87d8476bbc8516f3f5f790
Zenmarcky <zenvlog38@gmail.com>
2025-04-26 14:49:30 +0800

    good

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://myriadtechhub.netlify.app']
+    origin: ['http://localhost:3000', 'https://myriadtechhub.store']
 }));
 
 
